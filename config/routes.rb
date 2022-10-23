a3ba9a99271e47c5ef1405e201f4cd8e2c4c60c0
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-23 10:04:51 +0300

    polish routes to include login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :users
+  resources :users, only: [:index, :create, :show]
+  resources :categories, only: [:index]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # The below routes shall create a user session, authenticating them and log them out on requirement
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
