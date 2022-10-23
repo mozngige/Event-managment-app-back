88ae21160654c8e7fc02dbe3a56745c6f647052e
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-23 11:36:42 +0300

    defined the POST method and user-params function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,15 @@ rescue_from ActiveRecord::RecordNotFound, with: :unfound_response
         render json: user, status: :ok
     end
 
-    # def create
-    #     user = User.create!(user_params)
-    #     session[:user_id] = user.id
-    #     render json: user, status: :created
-    # end
+    def create
+        user = User.create!(user_params)
+        # session[:user_id] = user.id
+        render json: user, status: :created
+    end
 
-    # private
+    private
 
-    # def user_params
-    #     params.permit(:email,:username, :password, :is_organiser)
-    # end
+    def user_params
+        params.permit(:email,:username, :password, :password_confirmation, :is_organiser)
+    end
 end
