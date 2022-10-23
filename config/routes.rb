bb4077b2160634f33cf5bf1e0b0df16f4d357626
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-23 11:39:04 +0300

    added appropriate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show]
-  resources :communities
+  resources :communities, only: [:index, :create, :show]
   resources :abouts
   resources :user_events
-  resources :events
+  resources :events, only: [:create, :index, :show]
   resources :event_categories
   resources :user_profiles
-  resources :categories
+  resources :categories, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
