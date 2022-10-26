cf9267ee685784a028153ba968b1841565f963d7
Keith <keithwesley187@gmail.com>
2022-10-26 14:31:56 +0300

    milestone: added homebanners resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_095313) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_111518) do
   create_table "abouts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "about_img"
     t.text "about_description"
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_095313) do
     t.string "description"
     t.string "image_url1"
     t.string "image_url2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "home_banners", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
