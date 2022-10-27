c22d3d2b8c95fec3017af7555c66e263a617c8cd
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-27 23:58:35 +0300

    defined authorization before action method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  before_action :authorize
  
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
