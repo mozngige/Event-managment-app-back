7406e4c6184f6b4feb45d5aa6cde6eb5c54c5d9b
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-27 23:55:59 +0300

    error handling

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,6 @@
 class AuthController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_logged_in
+
     def authorize
 		user = User.find(session[:user_id])
 		if user.is_admin == true
@@ -6,5 +8,9 @@ class AuthController < ApplicationController
 		else
 			render json: {errors: ["Not authorized"]}, status: :unauthorized
 		end
+	end
+
+    def not_logged_in
+		render json: "Please log in first!", status: :not_found
 	end
 end
