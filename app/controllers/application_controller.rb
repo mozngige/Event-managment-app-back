47cc52223a339a42e25b4d3ce36e402393a9c0d1
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-28 00:28:11 +0300

    refactoring the before action code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-  before_action :authorize
+  before_action :authorized
  
+  
+  private
+  def authorized
+		@user = User.find_by(id: session[:user_id])
+		render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+	end
+
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
-
-  private
-  def authorize
-     return render json:{error: "Not authorized"}, status: :unauthorized unless session.include? :user_id 
-  end 
   
 end
