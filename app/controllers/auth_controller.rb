cd928d9cc9cb37908c0d82a9ef4842786db52a2d
JESKA-JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-10-27 23:53:48 +0300

    defined authorizztion function

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,3 +1,10 @@
 class AuthController < ApplicationController
-    
+    def authorize
+		user = User.find(session[:user_id])
+		if user.is_admin == true
+			render json: {success: 'authorized'}, status: :accepted
+		else
+			render json: {errors: ["Not authorized"]}, status: :unauthorized
+		end
+	end
 end
