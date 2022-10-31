3e0299acccecabbe735c7a9adbfa267fe8e5ee8d
Keith <keithwesley187@gmail.com>
2022-10-31 22:17:17 +0300

    milestone: fixing events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
     belongs_to :category
     has_many :tickets
     has_many :users,through: :tickets
-    validates :category_id, :title, :event_start_date, :event_end_date, :early_booking_end_date, :early_booking_price_regular, :early_booking_price_vip,:location , :regular_price, :vip_price, :vip_no_of_tickets, :regular_no_of_tickets, :banner_img, :description, :image_url1 , :image_url2, presence: true
+    validates :category_id, :title, :event_start_date, :event_end_date, :early_booking_end_date, :early_booking_price_regular, :early_booking_price_vip, :location , :regular_price, :vip_price, :vip_no_of_tickets, :regular_no_of_tickets, :banner_img, :description, :image_url1 , :image_url2, presence: true
 
     def time_diff
         current_time = Time.new
--- db/migrate/20221022030747_create_events.rb
+++ db/migrate/20221022030747_create_events.rb
@@ -1,13 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
-      t.integer :category_id
       t.string :title
       t.datetime :event_start_date
       t.datetime :event_end_date
       t.string :ticket_format
-      t.date :early_booking_end_date
-      t.time :early_booking_end_time
+      t.datetime :early_booking_end_date
       t.integer :early_booking_price_regular
       t.integer :early_booking_price_vip
       t.string :location
@@ -17,8 +15,9 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.integer :regular_no_of_tickets
       t.string :banner_img
       t.text :description
-      t.string :image_url1
-      t.string :image_url2
+      t.text :image_url1
+      t.text :image_url2
+      t.belongs_to :category, foreign_key: true
       
       t.timestamps
     end
